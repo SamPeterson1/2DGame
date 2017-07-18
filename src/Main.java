46b24b20f377f1b7785ca29421a64104eb16898c
Sam <Sam@192.168.1.108>
2017-07-17 20:43:29 -0500

    removed comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String args[]){
-		/* SAM IS NICE! */
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	    frame.add(new GameFrame());
